fe2f07a96082ba9e800d40775f112aaedd1fae25
Chou Hung-Yi <hychou.svm@gmail.com>
2025-03-11 21:48:02 +0800

    pop from queue with cli

--- src/qqabc_cli/main.py
+++ src/qqabc_cli/main.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+from typing import Annotated
 import typer
 from qqabc import JobQueueController
-from qqabc.types import NewSerializedJobRequest
+from qqabc.types import EmptyQueueError, NewSerializedJobRequest
 
 app = typer.Typer()
 
@@ -18,6 +19,22 @@ def submit(job_type: str, file_path: str):
     job = controller.add_job(request)
     print("job submitted")
     print(f"job id = {job.job_id}")
+
+@app.command()
+def pop(job_type: str, outdir: Annotated[str, typer.Option("-d")]):
+    if not Path(outdir).exists():
+        raise typer.BadParameter(f"Error: directory {outdir} does not exist")
+    if not Path(outdir).is_dir():
+        raise typer.BadParameter(f"Error: {outdir} is not a directory")
+
+    controller = JobQueueController()
+    try:
+        sjob = controller.get_next_job(job_type, deserialize=False)
+    except EmptyQueueError:
+        raise typer.BadParameter(f"Error: No job with job type: {job_type}")
+    output = Path(outdir) / f"{sjob.job_id}"
+    with open(output, "wb") as f:
+        f.write(sjob.job_body_serialized)
 
 @app.command()
 def _():
--- src/tests/cli/test_cli.py
+++ src/tests/cli/test_cli.py
@@ -1,56 +1,133 @@
+import os
+import re
 import tempfile
 import uuid
 from collections.abc import Generator
 from unittest.mock import patch
 
+import click
 import pytest
+from click.testing import Result as ClickResult
 from pytest_mock import MockerFixture
 from typer.testing import CliRunner
 
 from qqabc import JobQueueController
 from qqabc_cli.main import app
+from tests.fixtures.faker import Faker
 
 runner = CliRunner()
 
-BAD_ARG_EXIT_CODE = 2
-
-
-def test_submit_without_args() -> None:
-    result = runner.invoke(app, ["submit"])
-    assert result.exit_code == BAD_ARG_EXIT_CODE
+BAD_ARG_EXIT_CODE = click.UsageError.exit_code
 
 
 @pytest.fixture
-def fx_job_body_file() -> Generator[str, None, None]:
-    with tempfile.NamedTemporaryFile("wb") as f:
-        f.write(b"job_body")
+def fx_job_body_file(fx_faker: Faker) -> Generator[str, None, None]:
+    with tempfile.NamedTemporaryFile() as f:
+        f.write(fx_faker.json_bytes())
         f.flush()
         yield f.name
 
 
-def test_submit(fx_job_body_file: str, mocker: MockerFixture) -> None:
-    stub_uid = uuid.uuid4()
-    spy = mocker.spy(JobQueueController, "add_job")
-    with (
-        patch.object(uuid, "uuid4", return_value=stub_uid) as spy2,
-    ):
-        result = runner.invoke(app, ["submit", "my_job_type", fx_job_body_file])
-        assert result.exit_code == 0
-        assert "job submitted" in result.stdout
-        assert "job id = " in result.stdout
-        assert stub_uid.hex in result.stdout
-        assert spy.call_count == 1
-        assert spy2.call_count == 1
+def _get_stdout(result: ClickResult) -> str:
+    return re.sub(r"\s+", " ", re.sub(r"[\s╭─╮│╰╯]", " ", result.stdout))
 
 
-def test_submit_without_file() -> None:
-    result = runner.invoke(app, ["submit", "my_job_type"])
-    assert result.exit_code == BAD_ARG_EXIT_CODE
+class TestCli:
+    @pytest.fixture(autouse=True)
+    def setup_method(self, fx_faker: Faker) -> None:
+        self.fx_faker = fx_faker
 
+    def test_submit_without_args(
+        self,
+    ) -> None:
+        result = runner.invoke(app, ["submit"])
+        assert result.exit_code == BAD_ARG_EXIT_CODE
 
-def test_submit_with_invalid_file() -> None:
-    result = runner.invoke(app, ["submit", "my_job_type", "my_invalid_file.txt"])
-    assert result.exit_code == BAD_ARG_EXIT_CODE
-    assert "Error" in result.stdout
-    assert "my_invalid_file.txt" in result.stdout
-    assert "does not exist" in result.stdout
+    def test_submit(self, fx_job_body_file: str, mocker: MockerFixture) -> None:
+        stub_uid = uuid.uuid4()
+        spy = mocker.spy(JobQueueController, "add_job")
+        with (
+            patch.object(uuid, "uuid4", return_value=stub_uid) as spy2,
+        ):
+            result = runner.invoke(
+                app, ["submit", self.fx_faker.job_type(), fx_job_body_file]
+            )
+            assert result.exit_code == 0
+            stdout = _get_stdout(result)
+            assert "job submitted" in stdout
+            assert "job id = " in stdout
+            assert stub_uid.hex in stdout
+            assert spy.call_count == 1
+            assert spy2.call_count == 1
+
+    def test_submit_without_file(
+        self,
+    ) -> None:
+        result = runner.invoke(app, ["submit", self.fx_faker.job_type()])
+        assert result.exit_code == BAD_ARG_EXIT_CODE
+
+    def test_submit_with_invalid_file(
+        self,
+    ) -> None:
+        fname = self.fx_faker.file_name()
+        result = runner.invoke(app, ["submit", self.fx_faker.job_type(), fname])
+        assert result.exit_code == BAD_ARG_EXIT_CODE
+        stdout = _get_stdout(result)
+        assert "Error" in stdout
+        assert fname in stdout
+        assert "does not exist" in stdout
+
+    def test_pop_without_args(
+        self,
+    ) -> None:
+        result = runner.invoke(app, ["pop"])
+        assert result.exit_code == BAD_ARG_EXIT_CODE
+
+    def test_pop_from_empty_queue(self) -> None:
+        job_type = self.fx_faker.job_type()
+        with tempfile.TemporaryDirectory() as d:
+            result = runner.invoke(app, ["pop", job_type, "-d", d])
+        assert result.exit_code == BAD_ARG_EXIT_CODE
+        stdout = _get_stdout(result)
+        assert "Error" in stdout
+        assert "No job with job type" in stdout
+        assert job_type in stdout
+
+    def test_pop(self, fx_job_body_file: str) -> None:
+        stub_uid = uuid.UUID(self.fx_faker.uuid4())
+        job_type = self.fx_faker.job_type()
+        with patch.object(uuid, "uuid4", return_value=stub_uid):
+            runner.invoke(app, ["submit", job_type, fx_job_body_file])
+
+        with tempfile.TemporaryDirectory() as d:
+            result = runner.invoke(app, ["pop", job_type, "-d", d])
+            assert result.exit_code == 0
+            assert len(os.listdir(d)) == 1.0
+            job_file = os.path.join(d, stub_uid.hex)
+            assert os.path.exists(job_file)
+            with open(job_file, "rb") as f, open(fx_job_body_file, "rb") as f2:
+                assert f2.read() == f.read()
+
+    def test_pop_with_invalid_dir(self, fx_job_body_file: str) -> None:
+        job_type = self.fx_faker.job_type()
+        runner.invoke(app, ["submit", job_type, fx_job_body_file])
+        result = runner.invoke(
+            app, ["pop", job_type, "-d", dirname := self.fx_faker.file_name()]
+        )
+        assert result.exit_code == BAD_ARG_EXIT_CODE
+        stdout = _get_stdout(result)
+        assert "Error" in stdout
+        assert "does not exist" in stdout
+        assert "directory" in stdout
+        assert dirname in stdout
+
+    def test_pop_with_invalid_dir2(self, fx_job_body_file: str) -> None:
+        job_type = self.fx_faker.job_type()
+        runner.invoke(app, ["submit", job_type, fx_job_body_file])
+        with tempfile.NamedTemporaryFile() as f:
+            result = runner.invoke(app, ["pop", job_type, "-d", f.name])
+            assert result.exit_code == BAD_ARG_EXIT_CODE
+            stdout = _get_stdout(result)
+            assert "Error" in stdout
+            assert "is not a directory" in stdout
+            assert f.name in stdout
