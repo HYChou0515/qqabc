6741ce034a868b6a8c953b76084225d2f9016479
Chou Hung-Yi <hychou.svm@gmail.com>
2025-04-03 16:47:19 +0800

    add bdd test to add detail

--- tests/bdd/steps/test_post_status.py
+++ tests/bdd/steps/test_post_status.py
@@ -2,7 +2,6 @@ from pytest_bdd import scenario, given, when, then
 
 from tests.bdd.utils import create_a_job_file, create_a_job_online, get_job_id_by_submission_return, qqabc_cli
 
-from tests.tdd.cli.utils import ALL_STATUS
 from tests.utils import assert_result_success, assert_status, get_stdout
 
 
@@ -12,6 +11,9 @@ def test_post_one_status():
 @scenario('post_status.feature', '可以新增第多個status')
 def test_post_many_status():
     pass
+@scenario('post_status.feature', '新增status可以帶上detail')
+def test_post_status_with_detail():
+    pass
 
 @given("線上有一個job", target_fixture="job_id")
 def step(fx_workdir: str):
@@ -20,13 +22,13 @@ def step(fx_workdir: str):
     job_id = get_job_id_by_submission_return(r)
     return job_id
 
-@when("我post status", target_fixture="status")
+@when("我update status", target_fixture="status_and_detail")
 def step(job_id: str):
     r = qqabc_cli(["update", job_id, "-s", "running"])
     assert_result_success(r)
-    return "running"
+    return ("running", None)
 
-@when("我post status多次", target_fixture="status")
+@when("我update status多次", target_fixture="status_and_detail")
 def step(job_id: str):
     r = qqabc_cli(["update", job_id, "-s", "running"])
     assert_result_success(r)
@@ -34,12 +36,21 @@ def step(job_id: str):
     assert_result_success(r)
     r = qqabc_cli(["update", job_id, "-s", "success"])
     assert_result_success(r)
-    return "success"
+    return ("success", None)
+
+@when("update status with detail", target_fixture="status_and_detail")
+def step(job_id: str):
+    r = qqabc_cli(["update", job_id, "-s", "fail", "-d", "some detail"])
+    assert_result_success(r)
+    return ("fail", "some detail")
 
 @then("我可以看到這個status")
 @then("我可以看到最新的status")
-def step(job_id: str, status: str):
+def step(job_id: str, status_and_detail: tuple[str, str]):
+    status, detail = status_and_detail
     r = qqabc_cli(["get", "status", job_id])
     assert_result_success(r)
     stdout = get_stdout(r)
     assert_status(status, stdout)
+    if detail is not None:
+        assert detail in stdout
